a6d290ac6c60e8197f8a4ba7fcd4428f6ca315b3
“espinozam17” <130848535+Carlos171090@users.noreply.github.com>
2024-07-30 21:06:08 -0400

    entrega

---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCpmqiSICbzO_5yT7ROwMAaeTQd1jJTwpk",
+  authDomain: "desafio-f3a4c.firebaseapp.com",
+  projectId: "desafio-f3a4c",
+  storageBucket: "desafio-f3a4c.appspot.com",
+  messagingSenderId: "754485574632",
+  appId: "1:754485574632:web:21c6324c9c1244ab66025d",
+  measurementId: "G-6Q0TYFY0T7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app; 
